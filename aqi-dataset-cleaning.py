086236befe949c3331e5c8777895158f65535ce7
Divyansh0410-svg <divyanshpersonal2008@gmail.com>
2026-07-08 20:28:22 +0530

    Finding missing values and rows where average pollutant is 0

--- aqi-dataset-cleaning.py
+++ aqi-dataset-cleaning.py
@@ -8,4 +8,15 @@ aqi_data=pd.read_csv('AQI data set.csv')
 print(aqi_data.describe())
 aqi_data.info()
 
+print()
+
+# Check for missing values in the dataset
+print('Number of null values in each column:')
+print(aqi_data.isnull().sum().sort_values(ascending=False))
+
+print()
+
+#Checking for rows where average pollutant is 0
+zero_count=(aqi_data['pollutant_avg'] == 0).sum()
+print('Number of rows where average pollutant is 0: ', zero_count)
     
