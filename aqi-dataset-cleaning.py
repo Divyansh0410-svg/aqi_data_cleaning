c7b19a81eae757f71071bca8db79938a3a19f9bc
Divyansh0410-svg <divyanshpersonal2008@gmail.com>
2026-07-09 11:38:04 +0530

    Removed extra code and this is the final script

--- aqi-dataset-cleaning.py
+++ aqi-dataset-cleaning.py
@@ -4,43 +4,15 @@ import numpy as np
 # Load the AQI dataset
 aqi_data=pd.read_csv('AQI data set.csv')
 
-""" #Displaying statistical summary and information about the dataset
-print(aqi_data.describe())
-aqi_data.info() """
-
-
-""" #Checking for rows where average pollutant is 0
-zero_count=(aqi_data['pollutant_avg'] == 0).sum()
-print('Number of rows where average pollutant is 0: ', zero_count) """
-
 #Converting zero values in pollutant_avg column to NaN and than dropping those rows
 aqi_data['pollutant_avg'] = aqi_data['pollutant_avg'].replace(0, np.nan)
 aqi_data.dropna(subset=['pollutant_avg','pollutant_min','pollutant_max'], inplace=True)
 
 
-"""  #Verification after dropping rows
-print('Total remaining rows: ', len(aqi_data))
-print()
-print('Number of null values in each column after dropping rows:')
-print(aqi_data.isnull().sum().sort_values(ascending=False)) """
-
-""" #Detecting unwanted space in columns
-column_heads=['country','state','city','station','pollutant_id']
-for col in column_heads:
-    leading=aqi_data[col].astype(str).str.startswith(' ').sum()
-    tailing=aqi_data[col].astype(str).str.endswith(' ').sum()
-    if leading>0 or tailing>0:
-        print('Column',col,'has',leading,'rows with space in front and',tailing,'rows with space at end') """ 
-
 #Removing unwanted space from start and end in all string columns
 column_heads=['country','state','city','station','pollutant_id']
 for col in column_heads:
     aqi_data[col]=aqi_data[col].str.strip()
-
-""" #Verification after removing unwantes spaces
-leading=aqi_data['station'].astype(str).str.startswith(' ').sum()
-tailing=aqi_data['station'].astype(str).str.endswith(' ').sum()
-print('Column station','has',leading,'rows with space in front and',tailing,'rows with space at end') """
 
 #Converting last_update column from string to datetime
 aqi_data['last_update']= pd.to_datetime(aqi_data['last_update'], errors='coerce')
